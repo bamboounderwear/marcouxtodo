65d5d1b14b989f9a3a8727939a8360806d967c3e
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-01-29 23:20:05 -0500

    oacjage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import App from './App.tsx';
 import './index.css';
 
+// Simple reducer for react-beautiful-dnd
+const reducer = (state = {}, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
